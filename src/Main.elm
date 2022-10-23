19929730623eac9738a2675e88fb7e0d7c3b0804
bycepto <maxim.pertsov+ggyo@gmail.com>
2022-10-22 23:39:25 -0400

    init platformer

---
+++ src/Main.elm
@@ -0,0 +1,118 @@
+module Main exposing (main)
+
+-- import Canvas.Settings.Advanced as VA
+-- import Canvas.Settings.Text as VW
+-- import Canvas.Texture as VT
+
+import Browser exposing (Document)
+import Canvas as V
+import Canvas.Settings as VS
+import Color
+import Html as H
+import Html.Attributes as At
+
+
+
+-- PROGRAM
+
+
+main : Program Flags Model Msg
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    ()
+
+
+
+-- INITIALIZE
+
+
+type alias Flags =
+    {}
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( (), Cmd.none )
+
+
+
+-- UPDATE
+
+
+type alias Msg =
+    ()
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEWS
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Platformer"
+    , body = [ viewBody model ]
+    }
+
+
+viewBody : Model -> H.Html Msg
+viewBody _ =
+    H.div
+        [ At.style "display" "flex"
+        , At.style "justify-content" "center"
+        ]
+        [ viewGame ]
+
+
+viewGame : H.Html Msg
+viewGame =
+    V.toHtml
+        ( width, height )
+        [ At.style "border" "5px solid rgba(0,0,0,0.1)"
+        , At.style "display" "block"
+        ]
+        [ V.clear ( 0, 0 ) width height
+        , renderSquare
+        ]
+
+
+renderSquare : V.Renderable
+renderSquare =
+    V.shapes
+        [ VS.fill (Color.rgba 0 0 0 1) ]
+        [ V.rect ( 0, 0 ) 100 50 ]
+
+
+width : number
+width =
+    640
+
+
+height : number
+height =
+    480
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "elm-canvas";
+
+import { Elm } from "./Main.elm";
+
+Elm.Main.init({
+  node: document.getElementById("main"),
+  flags: {},
+});
